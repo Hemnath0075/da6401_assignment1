7588c2741d4319f66466dc857eb15faab3761fa9
hemnath0075 <hemnath5700@gmail.com>
2025-03-08 16:44:34 +0530

    udpates with base setup and question 1

---
+++ main.py
@@ -0,0 +1,38 @@
+import wandb
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import fashion_mnist
+
+# Initialize Weights & Biases
+wandb.init(project="da6401-ch24s016")
+
+# Load dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(train_labels)
+# Class names
+class_names = [
+    "T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+    "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
+]
+
+# Get one sample per class
+sample_images = []
+sample_labels = []
+
+for i in range(10):  # 10 classes in Fashion MNIST
+    index = np.where(train_labels == i)[0][0]  # Find the first occurrence of each class
+    sample_images.append(train_images[index])
+    sample_labels.append(class_names[i])
+
+# Create a figure and log to wandb
+fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+fig.suptitle("Fashion-MNIST Samples", fontsize=16)
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(sample_images[i], cmap="gray")
+    ax.set_title(sample_labels[i])
+    ax.axis("off")
+
+wandb.log({"fashion-mnist-samples": wandb.Image(fig)})
+plt.show()
